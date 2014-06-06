8b7d94c4d3b8a6665d8dfd510f5b77a1a78f2583
Mars17 <mars.soaresaraujo@gmail.com>
2014-06-06 20:07:46 -0300

    Considerando as funções que o PArticipanteDAO deverá ter;

--- Eventos/src/br/ifpi/dao/ParticipanteDAO.java
+++ Eventos/src/br/ifpi/dao/ParticipanteDAO.java
@@ -1,5 +1,13 @@
 package br.ifpi.dao;
+import java.util.List;
+
+import br.ifpi.entity.*;
 
 public interface ParticipanteDAO {
+	
+	public void addParticipante();
+	public Participante consultaParticipante(int cpf);
+	public List<Participante> consultarTodosParticipantes();	 
+	public void deleteParticipante( Participante participante);
 
 }
