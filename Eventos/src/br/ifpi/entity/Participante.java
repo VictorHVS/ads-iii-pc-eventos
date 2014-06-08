9b4663d22fe0c5e964e596f241148ca2e6917fef
Felipe Santos <phelipe.santos.1@gmail.com>
2014-06-08 09:14:59 -0300

    ** Alteração na Classse Participante (Inclusão de atributos métodos)

--- Eventos/src/br/ifpi/entity/Participante.java
+++ Eventos/src/br/ifpi/entity/Participante.java
@@ -1,5 +1,61 @@
 package br.ifpi.entity;
 
 public class Participante {
+	int id;
+	String nome;
+	String email;
+	String cpf;
+	String perfil;
+	
+	public Participante(){
+		
+	}
+	
+	public Participante(String _nome, String _email, String _cpf, String _perfil){
+		this.nome = _nome;
+		this.email = _email;
+		this.cpf = _cpf;
+		this.perfil = _perfil;
+	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getPerfil() {
+		return perfil;
+	}
+
+	public void setPerfil(String perfil) {
+		this.perfil = perfil;
+	}
+	
 }
