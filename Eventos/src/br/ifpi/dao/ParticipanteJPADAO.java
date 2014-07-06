c2732e014b6aac977e07136016ae911fde051b6a
Felipe Santos <phelipe.santos.1@gmail.com>
2014-07-06 03:10:54 -0300

    Arquivos de configuração do JPA

---
+++ Eventos/src/br/ifpi/dao/ParticipanteJPADAO.java
@@ -0,0 +1,63 @@
+package br.ifpi.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import br.ifpi.entity.Participante;
+
+public class ParticipanteJPADAO implements ParticipanteDAO{
+	EntityManager em = JPAUtil.getCurrentEntityManager();
+	
+	
+	@Override
+	public void addParticipante(Participante participante) {
+		
+		em.getTransaction().begin();
+		
+		em.persist(participante);
+		
+		em.getTransaction().commit();
+		
+		em.close();
+		
+	}
+
+	@Override
+	public Participante consultaParticipante(int cpf) {
+		
+		Query pesquisaPorCpf = em.createQuery("select p from Participante p where p.cpf = :cpf");
+		
+		pesquisaPorCpf.setParameter("cpf", cpf);
+		
+		Participante participante = (Participante)pesquisaPorCpf.getSingleResult();
+		
+		em.close();
+		
+		return participante;
+	}
+
+	@Override
+	public List<Participante> consultarTodosParticipantes() {
+		
+		Query pesquisaTodosParticipantes = em.createQuery("select p from Participante p");
+		
+		List<Participante> listaParticipante = pesquisaTodosParticipantes.getResultList();
+		
+		em.close();
+		
+		return listaParticipante;
+	}
+
+	@Override
+	public void deleteParticipante(Participante participante) {
+		em.getTransaction().begin();
+		
+		em.remove(participante);
+		
+		em.getTransaction().commit();
+		
+	}
+
+}
