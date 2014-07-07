c5713392273fd81ff18f0ef4068948341804ff1a
Mars Soares <Mars Soares@Mars>
2014-07-07 17:49:52 -0300

    parte não comitada

---
+++ Eventos/src/main/java/br/ifpi/entity/SegmentoParticipante.java
@@ -0,0 +1,37 @@
+package br.ifpi.entity;
+
+public class SegmentoParticipante {
+	private int id;
+	private String nome;
+	private CupomPromocional cupomDesconto;
+	
+	public SegmentoParticipante(){
+		
+	}
+
+	public CupomPromocional getCupomDesconto() {
+		return cupomDesconto;
+	}
+
+	public void setCupomDesconto(CupomPromocional cupomDesconto) {
+		this.cupomDesconto = cupomDesconto;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
+}
